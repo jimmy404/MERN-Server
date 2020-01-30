c4004bcf303de6ee28f8fc0cb96a5d933a0f7862
Jimmy <bruno_pier@hotmail.com>
2020-01-30 18:05:53 -0300

    Adding code for homepage - req-res - index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
+//definir pag principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+});
+
 //arrancar serv
 app.listen(PORT, () => {
     console.log(`El servidor esta funcionando en puerto ${PORT}`);
