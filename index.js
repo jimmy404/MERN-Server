b8245110bf6c1140dff52a97ecbf5de8cde21117
Jimmy <bruno_pier@hotmail.com>
2020-01-30 19:57:27 -0300

    Adding conectarDB - index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 //crear el serv
 const app = express();
 
+//conectar a base de datos
+conectarDB();
 
 // puerto de la app
 const PORT = process.env.PORT || 4000;
