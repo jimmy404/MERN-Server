2dcf0cdbcd52d32fb9472e490403764de66e9f16
Jimmy <bruno_pier@hotmail.com>
2020-01-31 19:29:02 -0300

    Adding router.get code - proyectos.js

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const proyectoController = require('../controllers/proyectocontroller');
+const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
 
 
 //crear proyecto
 // api/proyectos
 router.post('/',
+    auth,
     proyectoController.crearProyecto
 )
+
+router.get('/',
+    auth,
+    proyectoController.crearProyecto
+)
+
 
 module.exports = router;
