6281c71ce9ad732706a895b604e1e853dbdaa5b6
Jimmy <bruno_pier@hotmail.com>
2020-01-31 19:04:13 -0300

    Adding new proyectos code - proyectoController.js

--- controllers/proyectocontroller.js
+++ controllers/proyectocontroller.js
@@ -1,3 +1,14 @@
-exports.crearProyecto = async (req, res) => {
+const Proyecto = require('../models/Proyecto');
 
+
+exports.crearProyecto = async (req, res) => {
+    try {
+        //crear proyecto nuevo
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+        res.json(escape);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
