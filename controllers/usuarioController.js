3e0b3a8be6d0a226a698c915c2ac2e9dfe066572
Jimmy <bruno_pier@hotmail.com>
2020-01-31 01:04:01 -0300

    Adding code to show message in case of repeated data - userController.js

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,8 +2,16 @@ const Usuario = require('../models/Usuario');
 
 exports.crearUsuario = async (req, res) => {
 
+    //extraer email y pass
+    const {email, password} = req.body;
+
     try {
-        let usuario;
+        //revisar unico usuario
+        let usuario = await Usuario.findOne({email});
+
+        if(usuario){
+            return res.status(400).json({msg: 'El usuario ya existe'})
+        }
 
         //crea el nuevo usuario
         usuario = new Usuario(req.body);
@@ -11,7 +19,7 @@ exports.crearUsuario = async (req, res) => {
         await usuario.save();
 
         //mensaje de confirmacion
-        res.send('Usuario creado correctamente')
+        res.json({msg: 'Usuario creado correctamente'})
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
