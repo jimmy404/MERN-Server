438172d8ce185ee820fa50765252d1ad542a6785
Jimmy <bruno_pier@hotmail.com>
2020-01-31 00:51:19 -0300

    Adding new folder&file & trycatch code - controller - usuarioController.js

---
+++ controllers/usuarioController.js
@@ -0,0 +1,19 @@
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+
+    try {
+        let usuario;
+
+        //crea el nuevo usuario
+        usuario = new Usuario(req.body);
+        //guarda usuario
+        await usuario.save();
+
+        //mensaje de confirmacion
+        res.send('Usuario creado correctamente')
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
+}
