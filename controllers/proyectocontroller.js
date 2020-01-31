ac436d2ec98d5b1b055faff2966f1e9887534ac4
Jimmy <bruno_pier@hotmail.com>
2020-01-31 19:29:40 -0300

    Adding trycatch code&rules - proyectosController.js

--- controllers/proyectocontroller.js
+++ controllers/proyectocontroller.js
@@ -5,8 +5,14 @@ exports.crearProyecto = async (req, res) => {
     try {
         //crear proyecto nuevo
         const proyecto = new Proyecto(req.body);
+
+        //guardar el creador via jwt
+        proyecto.creador = req.usuario.id;
+
+        //guardamos el proyecto
         proyecto.save();
-        res.json(escape);
+        res.json(proyecto);
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
