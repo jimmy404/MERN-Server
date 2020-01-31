415aa2201506d06b5d0048dbfbbc5845a75ba5cc
Jimmy <bruno_pier@hotmail.com>
2020-01-31 20:25:36 -0300

    Adding code to Update Projects with PUT - proyectocontroller.js

--- controllers/proyectocontroller.js
+++ controllers/proyectocontroller.js
@@ -38,3 +38,33 @@ exports.obtenerProyectos = async (req, res) => {
         res.status(500).send('Hubo un error');
     }
 }
+
+//Actualiza un proyecto
+exports.actualizarProyecto = async(req, res) => {
+
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()})
+    }
+
+    //extraer la informacion del proyecto
+    const {nombre} = req.body;
+    const nuevoProyecto = {};
+
+    if(nombre) {
+        nuevoProyecto.nombre = nombre;
+    }
+
+    try {
+        //revisar id
+
+        //si existe el proyecto o no
+
+        //verificar el creador del proyecto - solo usuario autenticado
+
+        //actualizar
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error en el servidor');
+    }
+}
