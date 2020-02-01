3ddd516cc27832b15228d0b5571beb82dc85dcd0
Jimmy <bruno_pier@hotmail.com>
2020-02-01 19:51:29 -0300

    Adding router.post code & export - tareas.js

--- routes/tareas.js
+++ routes/tareas.js
@@ -3,3 +3,15 @@ const router = express.Router();
 const tareaController = require('../controllers/tareaController');
 const auth = require('../middleware/auth');
 const { check } = require('express-validator');
+
+//crear tarea
+//api/tarea
+router.post('/',
+    auth,
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty()
+    ],
+    tareaController.crearTarea
+);
+
+module.exports = router;
