124a4b57bcb1ad70c7002d8ed5ad8e0d3ac86488
Jimmy <bruno_pier@hotmail.com>
2020-02-01 20:51:43 -0300

    Adding code to get task by project - obtenerTareas - trycatch - tareaController.js

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -35,3 +35,27 @@ exports.crearTarea = async (req, res) => {
         res.status(500).send('Hubo un error')
     }
 }
+
+//obtiene tareas por proyecto
+exports.obtenerTareas = async (req, res) => {
+    try {
+        //extraer proyecto y comprobar si existe
+        const { proyecto } = req.body;
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if(!existeProyecto){
+            return res.status(404).json({msg: 'Proyecto no encontrado'})
+        }
+
+        //revisar si el proyecto pertenece al usuario autenticado
+        if( existeProyecto.creador.toString() !== req.usuario.id ){
+            return res.status(401).json({msg: 'No autorizado'});
+        }
+
+        //obtener las tareas por proyecto
+        const tareas = await Tarea.find({proyecto});
+        res.json({tareas});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
