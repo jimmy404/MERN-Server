b2ff52d9fac604475d16dc3e2196b6982e5e60f6
Jimmy <bruno_pier@hotmail.com>
2020-02-01 19:51:53 -0300

    Adding crearTarea & const code - tareaController.js

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,8 +1,13 @@
 const Tarea = require('../models/Tarea');
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
 
 //Crea nueva tarea
 
 exports.crearTarea = async (req, res) => {
-    
+
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()})
+    }
 }
