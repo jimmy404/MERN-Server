14111e64b60538f74ea1683abcf3b6f484a1f028
Jimmy <bruno_pier@hotmail.com>
2020-02-01 20:39:46 -0300

    Renaming const proyecto to existeProyecto - tareaController.js

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -11,14 +11,25 @@ exports.crearTarea = async (req, res) => {
         return res.status(400).json({errores: errores.array()})
     }
 
-    //extraer proyecto y comprobar si existe
-    const { proyecto } = req.body;
 
     try {
-        const proyecto = await Proyecto.findById(proyecto);
-        if(!proyecto){
+        //extraer proyecto y comprobar si existe
+        const { proyecto } = req.body;
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if(!existeProyecto){
             return res.status(404).json({msg: 'Proyecto no encontrado'})
         }
+
+        //revisar si el proyecto pertenece al usuario autenticado
+        if( existeProyecto.creador.toString() !== req.usuario.id ){
+            return res.status(401).json({msg: 'No autorizado'});
+        }
+
+        //creamos la tarea
+        const tarea = new Tarea(req.body);
+        await tarea.save();
+        res.json({tarea});
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error')
