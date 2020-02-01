bb2d4670c3b5ee2974bd9f970dd32fc77b32fe32
Jimmy <bruno_pier@hotmail.com>
2020-02-01 19:40:43 -0300

    Adding new file & initial code - tarea.js

---
+++ routes/tareas.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const tareaController = require('../controllers/tareaController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
